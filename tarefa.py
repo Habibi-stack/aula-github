6a45cf12a18164d571cc09ff8f0b87cf201486a3
alan <aluno@IOT.MAUES.LOCAL>
2026-03-25 07:52:19 -0400

    terceira modificação

--- tarefa.py
+++ tarefa.py
@@ -3,5 +3,5 @@ print("aula do git")
 nota1 = int(input("digite sua primeira nota:\n"))
 nota2= int (input("digite sua segunda nota:\n"))
 nota3= int(input("digite sua segunda nota:\n"))
-media=(nota1+nota2+nota3)/2
+media=(nota1+nota2+nota3)/3
 print(media)
