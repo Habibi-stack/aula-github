154b9472854f6daff5be7b95652d159cf61f1cfb
Cetam Maues Aluno <aluno@IOT.MAUES.LOCAL>
2026-03-25 07:47:08 -0400

    inserindo o primeiro arquivo

--- tarefa.py
+++ tarefa.py
@@ -1,6 +1,7 @@
 print("aula do git")
 
 nota1 = int(input("digite sua primeira nota:\n"))
-nota2= int(input("digite sua segunda nota:\n"))
-media=(nota1+nota2)/2
+nota2= int (input("digite sua segunda nota:\n"))
+nota3= int(input("digite sua segunda nota:\n"))
+media=(nota1+nota2+nota3)/2
 print(media)
